c76e1cc70816d99817d1b8c9086204575f155acf
Axxell04-UTM <alajones1404@utm.edu.ec>
2025-06-02 17:10:56 -0500

    Añadir protección de ruta para las páginas de deudas y préstamos, y corregir el manejo de eventos en la página de deudas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage),
   },
   {
     path: 'register',
@@ -22,18 +22,22 @@ export const routes: Routes = [
   },
   {
     path: 'find-the-diamond',
-    loadComponent: () => import('./find-the-diamond/find-the-diamond.page').then( m => m.FindTheDiamondPage)
+    loadComponent: () => import('./find-the-diamond/find-the-diamond.page').then( m => m.FindTheDiamondPage),
+    canActivate: [AuthGuard]
   },
   {
     path: 'guess-the-way',
-    loadComponent: () => import('./guess-the-way/guess-the-way.page').then( m => m.GuessTheWayPage)
+    loadComponent: () => import('./guess-the-way/guess-the-way.page').then( m => m.GuessTheWayPage),
+    canActivate: [AuthGuard]
   },
   {
     path: 'debts',
-    loadComponent: () => import('./debts/debts.page').then( m => m.DebtsPage)
+    loadComponent: () => import('./debts/debts.page').then( m => m.DebtsPage),
+    canActivate: [AuthGuard]
   },
   {
     path: 'loans',
-    loadComponent: () => import('./loans/loans.page').then( m => m.LoansPage)
+    loadComponent: () => import('./loans/loans.page').then( m => m.LoansPage),
+    canActivate: [AuthGuard]
   },
 ];
